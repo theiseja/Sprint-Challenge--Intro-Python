0dd299a2f0a2ea6a4020ad0463a9c2aa6dbbb9a0
Jesse Theisen <theiseja@gmail.com>
2020-01-26 21:02:02 -0600

    Finishes cityreader and Stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,17 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+import csv
 
+
+# City class
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self. lat = lat
+    self.lon = lon
+    
+  def __repr__(self):
+    return f'{self.name}: {self.lat}, {self.lon}'
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -20,9 +31,16 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
-    return cities
-
+  csvfile = open('../../src/cityreader/cities.csv', 'r')
+  cityRead = csv.DictReader(csvfile)
+  for row in cityRead:
+    floatLat = float(row['lat'])
+    floatLon = float(row['lng'])
+    newCity = City(row['city'], floatLat, floatLon)
+    cities.append(newCity)
+  csvfile.close()
+  
+  return cities
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
@@ -63,6 +81,12 @@ for c in cities:
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
   # within will hold the cities that fall within the specified region
   within = []
+  latMax = max(lat1, lat2)
+  latMin = min(lat1, lat2)
+  lonMax = max(lon1, lon2)
+  lonMin = min(lon1, lon2)
+  
+  within = [city for city in cities if city.lat >= latMin and city.lat <= latMax and city.lon >= lonMin and city.lon <= lonMax]
 
   # TODO Ensure that the lat and lon valuse are all floats
   # Go through each city and check to see if it falls within 
